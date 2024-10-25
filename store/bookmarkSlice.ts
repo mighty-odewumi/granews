9cce6cf8647ded27149fedd4ce4776163ea3e931
gr1ntch <odewumimighty@gmail.com>
2024-10-25 06:19:42 +0100

    fix: Successfully fixed bookmark bug with Redux Toolkit

---
+++ store/bookmarkSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+interface BookmarkState {
+  bookmarkedArticles: string[]
+}
+
+const initialState: BookmarkState = {
+  bookmarkedArticles: []
+}
+
+const bookmarkSlice = createSlice({
+  name: "bookmark",
+  initialState,
+  reducers: {
+    setBookmarks: (state, action: PayloadAction<string[]>)  => {
+      state.bookmarkedArticles = action.payload;
+    },
+    toggleBookmark: (state, action: PayloadAction<string>) => {
+      const articleId = action.payload;
+      const index = state.bookmarkedArticles.indexOf(articleId)
+      if (index > -1) {
+        state.bookmarkedArticles.splice(index, 1);
+      } else {
+        state.bookmarkedArticles.push(articleId);
+      }
+
+      AsyncStorage.setItem("bookmarks", JSON.stringify(state.bookmarkedArticles));
+    }
+  }
+});
+
+export const { setBookmarks, toggleBookmark } = bookmarkSlice.actions;
+
+// Thunk to load bookmarks from AsyncStorage
+export const loadBookmarks = () => async (dispatch: any) => {
+  try {
+    const bookmarksJson = await AsyncStorage.getItem("bookmarks");
+    if (bookmarksJson) {
+      const bookmarks = JSON.parse(bookmarksJson);
+      dispatch(setBookmarks(bookmarks));
+    }
+  } catch(error) {
+    console.error("Failed to load bookmarks", error);
+  }
+}
+
+export default bookmarkSlice.reducer;
